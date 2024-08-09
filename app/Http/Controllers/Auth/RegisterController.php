6ed3bb7ca912c4998e07ab8c6e1c940d44db965e
radenmasabdul <abdul.tamsis@gmail.com>
2024-08-09 22:50:01 +0700

    feat: create restfull api register

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle user registration.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    public function register(Request $request){
+
+        //validation
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:225',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'role' => 'required|string|in:administrator, userHQ, userBranch',
+        ]);
+
+        //if validation fails
+        if($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        //create user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+        //return json success
+        if($user) {
+            return response()->json([
+                'success' => true,
+                'user'    => $user,
+            ], 201);
+        }
+
+        //return json failed
+        return response()->json([
+            'success' => false,
+        ], 409);
+    }
+
+}
