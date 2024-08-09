54ea5dc80191ecc1d75a74ec35135e18ae477036
radenmasabdul <abdul.tamsis@gmail.com>
2024-08-09 21:53:15 +0700

    feat: configure jwt

--- config/auth.php
+++ config/auth.php
@@ -36,9 +36,13 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
+    'web' => [
+        'driver' => 'session',
+        'provider' => 'users',
+        ],
+    'api' => [
+        'driver' => 'jwt',
+        'provider' => 'users',
         ],
     ],
 
